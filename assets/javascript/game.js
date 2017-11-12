073436401e51de12f9a1a20f6d78507ef223fb9b
Lisa Vinson <Lrv0828@gmail.com>
2017-11-12 00:04:42 -0500

    Completed html, css draft, declared variables in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//Declare variables:
+
+var total = 0;
+var wins = 0;
+var losses = 0;
+var randomNumber
+ 
+
+//When page starts up, generates a new number
+    //Set random number = (19 -120)
+
+//Assigns value to each crystal (1 - 12)
+
+
+//Total starts at 0, and then increases by the value of the crystal clicked
+
+//After each click, runs a test:
+
+
+//if total number = random number: end game/restart
+
+//if total number > random number: end game/restart
